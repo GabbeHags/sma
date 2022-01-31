8daae77aa8c7a60cddb9c4201611e81d1d412e83
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2022-01-31 03:12:20 +0100

    update

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,13 @@
 use std::process::{Command, Stdio};
 use crate::{get_paths, parse_args};
 
+fn program_path() -> String{
+    r"test_program\hello_world.exe".to_string()
+}
+
 #[test]
 fn program_test() {
-    let args = vec!["self", "--start", r"test_program\hello_world.exe"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--start", program_path().as_str()].iter().map(|arg| arg.to_string()).collect();
     let (start, _exit) = match parse_args(args) {
         Ok(r) => {r}
         Err(e) => {panic!("{e}")}
@@ -23,7 +27,7 @@ fn program_test() {
 
 #[test]
 fn parse_args_test1() {
-    let args = vec!["self", "--start", r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "2", "3", "--exit",  "1"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--start", program_path().as_str(), "2", "3", "--exit",  "1"].iter().map(|arg| arg.to_string()).collect();
     let (start, exit) = match parse_args(args) {
         Ok(r) => {r}
         Err(e) => {panic!("{e}")}
@@ -33,15 +37,15 @@ fn parse_args_test1() {
 }
 #[test]
 fn parse_args_test2() {
-    let args = vec!["self", "--star", r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "2", "3", "--exit",  "1"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--star", program_path().as_str(), "2", "3", "--exit",  "1"].iter().map(|arg| arg.to_string()).collect();
     assert!(parse_args(args).is_err());
-    let args = vec!["self", "--start", r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "2", "3", "--exi",  "1"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--start", program_path().as_str(), "2", "3", "--exi",  "1"].iter().map(|arg| arg.to_string()).collect();
     assert!(parse_args(args).is_err());
 }
 
 #[test]
 fn get_paths_test1() {
-    let args = vec!["self", "--start", r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--start", program_path().as_str()].iter().map(|arg| arg.to_string()).collect();
     let (start, _exit) = parse_args(args).unwrap();
     let paths = match get_paths(start) {
         Ok(r) => {r}
@@ -51,10 +55,10 @@ fn get_paths_test1() {
 }
 #[test]
 fn get_paths_test2() {
-    let args = vec!["self", "--start", r"C:\Program Files (x86)\Google\Chrome\Application\chrom.exe"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--start", r"test_program\hello_worl.exe"].iter().map(|arg| arg.to_string()).collect();
     let (start, _exit) = parse_args(args).unwrap();
     assert!(get_paths(start).is_err());
-    let args = vec!["self", "--start", r"C:\Program Files (x86)\Google\Chrome\Application\chrome.xe"].iter().map(|arg| arg.to_string()).collect();
+    let args = vec!["self", "--start", r"test_program\hello_world.ex"].iter().map(|arg| arg.to_string()).collect();
     let (start, _exit) = parse_args(args).unwrap();
     assert!(get_paths(start).is_err());
 }
