90377281f2cd4ee8dc83b101a78d591ed04a438b
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2023-04-06 17:50:54 +0200

    .

--- gui/src/routes/+page.svelte
+++ gui/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+  import { invoke } from '@tauri-apps/api/tauri';
+  export let name: string = 'aaab';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+Hello {name}
