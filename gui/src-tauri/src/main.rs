8062b19dcc03255609e53040da8d7b7f752b6d06
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2023-04-09 04:31:53 +0200

    .

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -3,8 +3,23 @@
     windows_subsystem = "windows"
 )]
 
+#[cfg(debug_assertions)]
+use tauri::Manager;
+
 fn gui() -> anyhow::Result<()> {
-    tauri::Builder::default().run(tauri::generate_context!())?;
+    tauri::Builder::default()
+        .setup(
+            |#[cfg_attr(not(debug_assertions), allow(unused_variables))] app| {
+                #[cfg(debug_assertions)] // only include this code on debug builds
+                {
+                    let window = app.get_window("main").unwrap();
+                    window.open_devtools();
+                    window.close_devtools();
+                }
+                Ok(())
+            },
+        )
+        .run(tauri::generate_context!())?;
     Ok(())
 }
 
--- gui/src/routes/+page.svelte
+++ gui/src/routes/+page.svelte
@@ -1,9 +1,146 @@
 <script lang="ts">
   import { invoke } from '@tauri-apps/api/tauri';
-  export let name: string = 'aaab';
+  import { open, save } from '@tauri-apps/api/dialog';
+
+  async function load_config_file() {
+    let path = <string | null>await open({
+      multiple: false,
+      filters: [
+        {
+          name: 'sma config',
+          extensions: ['json']
+        }
+      ]
+    });
+    if (path === null) {
+      return;
+    }
+    config_path = path;
+  }
+  async function save_config_file() {
+    let path = <string | null>await save({
+      filters: [
+        {
+          name: 'sma config',
+          extensions: ['json']
+        }
+      ]
+    });
+    if (path === null) {
+      return;
+    }
+    config_path = path;
+  }
+
+  function generate_shortcut_with_config() {}
+
+  function disable_right_click() {
+    document.addEventListener(
+      'contextmenu',
+      (e) => {
+        e.preventDefault();
+        return false;
+      },
+      { capture: true }
+    );
+  }
+  let config_path: string = '';
+
+  let starts: string[] = [''];
+
+  async function get_start_with_dialog(index: number) {
+    let path = <string | null>await open({
+      multiple: false,
+      filters: [
+        {
+          name: '',
+          extensions: ['exe', 'shortcut']
+        }
+      ]
+    });
+    if (path === null) {
+      return;
+    }
+    starts[index] = path;
+  }
+
+  disable_right_click();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+  <header>
+    <h1>SMA</h1>
+  </header>
 
-Hello {name}
+  <body>
+    <form>
+      <label>
+        <div class="sub-title" id="config-start-title">Start</div>
+        {#each starts as path, index}
+          <label>
+            <div>
+              {index + 1}.
+              <input type="text" bind:value={path} />
+              <button on:click={() => get_start_with_dialog(index)}>Find</button>
+              {#if starts.length > 1}
+                <button
+                  on:click={() => {
+                    starts.splice(index, 1);
+                    starts = starts;
+                  }}>X</button
+                >
+              {/if}
+            </div>
+          </label>
+        {/each}
+        <div>
+          <button
+            on:click={() => {
+              starts.push('');
+              starts = starts;
+            }}>+</button
+          >
+        </div>
+      </label>
+      <label>
+        <div class="sub-title" id="config-options-title">Options</div>
+        <label>
+          Cascade kill: <input type="checkbox">
+        </label>
+      </label>
+    </form>
+
+    <div id="load-save-buttons">
+      <button id="config-file-load-button" on:click={load_config_file}>Load config</button>
+      <button id="config-file-save-button" on:click={save_config_file}>Save config</button>
+    </div>
+    <button id="generate-shortcut-button" on:click={generate_shortcut_with_config}>
+      Generate shortcut
+    </button>
+  </body>
+
+  <footer>Created by GabbeHags</footer>
+</main>
+
+<style>
+
+  .sub-title {
+    font-size: 20px;
+  }
+
+
+  #load-save-buttons {
+    padding-bottom: 1%;
+  }
+
+  main {
+    text-align: center;
+  }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 20px;
+  }
+</style>
