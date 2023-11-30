01b7fd5c43f9af02c4e08cf1bed4b0764f9f1629
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2023-11-30 22:36:26 +0100

    update

--- release.sh
+++ release.sh
@@ -1,1 +1,6 @@
-cargo tauri build -- --bin sma-gui && mkdir -p ./target/prod-release-bundle && cp ./target/release/sma.exe ./target/release/sma-gui.exe ./target/release/bundle/msi/sma-gui* ./target/prod-release-bundle
+set -e
+
+cargo test --workspace
+cargo tauri build -- --bin sma-gui
+mkdir -p ./target/prod-release-bundle
+cp ./target/release/sma.exe ./target/release/sma-gui.exe ./target/release/bundle/msi/sma-gui* ./target/prod-release-bundle
